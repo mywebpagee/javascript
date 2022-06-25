abe63180bcf83c1f19e112234c28f116e2a7b80a
Papiy_d <92183561+mywebpagee@users.noreply.github.com>
2022-06-25 12:02:53 +0100

    Add files via upload

---
+++ javascript/js/print.js
@@ -0,0 +1,9 @@
+let name = "What is your name";
+let height = "What is your height";
+let country = "Enter your country";
+
+document.write(prompt(name));
+document.write("<br>");
+document.write(prompt(height));
+document.write("<br>");
+document.write(prompt(country));
